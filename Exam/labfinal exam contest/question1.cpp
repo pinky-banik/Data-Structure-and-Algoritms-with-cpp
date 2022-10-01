fc1f6ce6aa01287fa55e02b5500d2b16d92ede38
pinky-banik <pinky.rani.banik@gmail.com>
2022-10-01 19:45:07 +0600

    binary tree

--- Exam/labfinal exam contest/question1.cpp
+++ Exam/labfinal exam contest/question1.cpp
@@ -1,8 +1,37 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
+ 
 int main(){
-  int a;
- cin>>a;
- cout << a;
+ 
+   int t;
+   cin>>t;
+ 
+   for(int i = 0 ; i < t; i++){
+       int n, c;
+       cin>>n>>c;
+ 
+       int arr[n];
+        int flag = 0;
+       for(int j = 0; j < n; j++){
+           cin>>arr[j];
+       }
+ 
+       for(int j = 0; j < n; j++){
+           c= arr[j]*2;
+ 
+       }
+       if(c > 0) flag = 1;
+ 
+ 
+ 
+           if(flag == 1){
+               cout<<"Yes"<<endl;
+           }
+           else{
+               cout<<"No"<<endl;
+           }
+   }
+ 
+ 
+return 0;
 }
